beb902c6f6d4f563aa5e298f934e520e7981a49d
“Janetgit config --global user.email janet.chang.0110@gmail.com <janet.chang.0110@gmail.com>
2026-05-09 15:41:41 +1000

    develop signalHandler

--- animate_server.c
+++ animate_server.c
@@ -2,15 +2,29 @@
 #include "animate/animate.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
+
+void signalHandler(int sig, siginfo_t *info, void *ucontext){
+    pid_t client_pid = info->si_pid;
+    kill(client_pid, SIGUSR2);
+}
 
 int main(int argc, char** argv, char** envp) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         return 1;
     }
-    
+
+    //get server pid
     pid_t pid = getpid();
     printf("Server PID: %d.\n", pid);
+
+    //after receiving the signal from the client
+    struct sigaction sa;
+    signal(SIGUSR1, signalHandler);
+    
+
+
     struct canvas* canvas = animate_create_canvas(100,100,0);
     animate_destroy_canvas(canvas);
     
