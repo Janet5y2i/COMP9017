b8467618e351b2694fe9e68510c3d6fabaf24d19
“Janetgit config --global user.email janet.chang.0110@gmail.com <janet.chang.0110@gmail.com>
2026-05-09 15:12:59 +1000

    build the bridge of client and server

--- animate_server.c
+++ animate_server.c
@@ -1,8 +1,16 @@
 
-#include <animate/animate.h>
+#include "animate/animate.h"
+#include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char** argv, char** envp) {
-
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+    
+    pid_t pid = getpid();
+    printf("Server PID: %d.\n", pid);
     struct canvas* canvas = animate_create_canvas(100,100,0);
     animate_destroy_canvas(canvas);
     
