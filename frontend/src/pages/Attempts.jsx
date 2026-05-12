690f7aea2399b0228d3925dde591bf22bc6175e5
wcha0938 <wcha0938@uni.sydney.edu.au>
2026-05-12 22:26:06 +1000

    login function update

--- frontend/src/pages/Attempts.jsx
+++ frontend/src/pages/Attempts.jsx
@@ -105,7 +105,7 @@ export default function Attempts() {
                 {score.time.toLocaleString('en-au')}
               </span>
             </li>
-          ))}
+          ))}œ
         </ul>
       </section>
     );
