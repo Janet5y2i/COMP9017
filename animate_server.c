550c1033a2cc3288d69b322a2c38a47bcf51a35a
“Janetgit config --global user.email janet.chang.0110@gmail.com <janet.chang.0110@gmail.com>
2026-05-12 18:51:40 +1000

    FIFO developed

--- animate_server.c
+++ animate_server.c
@@ -3,11 +3,43 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <errno.h>
 
+#define BUFF 1024
 
 void signalHandler(int sig, siginfo_t *info, void *ucontext){
     pid_t client_pid = info->si_pid;
     printf("Signal received from: %d.\n", client_pid);
+    char path_c2s[BUFF];
+    char path_s2c[BUFF];
+    //set FIFO path by id
+    sprintf(path_c2s, "FIFO_C2S_%d", client_pid);
+    sprintf(path_s2c, "FIFO_S2C_%d", client_pid);
+
+    int res_c2s = mkfifo(path_c2s, 0666);
+    int res_s2c = mkfifo(path_s2c, 0666);
+
+    //remove the exist one if exist
+    if (res_c2s == -1){
+        if (errno == EEXIST){
+            unlink(path_c2s);
+            mkfifo(path_c2s, 0666);
+        } else {
+            perror("mkfifo");
+        }
+    }
+
+
+    if (res_s2c == -1){
+        if (errno == EEXIST){
+            unlink(path_s2c);
+            mkfifo(path_s2c, 0666);
+        } else {
+            perror("mkfifo");
+        }
+    }
+    //make FIFO
+
     kill(client_pid, SIGUSR2);
 }
 
@@ -31,13 +63,8 @@ int main(int argc, char** argv, char** envp) {
     sa.sa_sigaction = signalHandler;
 
 
-    //first, send sigusr1 to client
+    //receiving sigusr1 from client,  send FIFO and sigusrs to client
     sigaction(SIGUSR1, &sa, NULL);
-    
-    
-    
-
-
     
     
     //prevent the code end before receiving reply
