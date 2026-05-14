828e894cb910714a37380d9107c4d5c76be49795
“Janetgit config --global user.email janet.chang.0110@gmail.com <janet.chang.0110@gmail.com>
2026-05-15 07:24:44 +1000

    RPC

--- animate_client.c
+++ animate_client.c
@@ -74,8 +74,10 @@ int main(int argc, char** argv, char** envp) {
     sprintf(path_c2s, "FIFO_C2S_%d", client_pid);
     sprintf(path_s2c, "FIFO_S2C_%d", client_pid);
 
-    fgets(req, BUFF, stdin);
     //send login message to server
+    //get login cmd from user
+    fgets(req, BUFF, stdin);
+    //get username from stdin
     sscanf(req, "Login %s\n", username);
     
     //open fifo
@@ -85,18 +87,33 @@ int main(int argc, char** argv, char** envp) {
 
 
     write(fd_c2s, req, strlen(req));
+    //get auth response
 
     ssize_t size_read = read(fd_s2c, res, sizeof(res)-1);
     if ( size_read > 0) {
         res[size_read] = '\0';
         if ((strstr(res, "Reject") != NULL)){
             printf("%s\n", res);
-            //close(fd_c2s);
-            //close(fd_s2c);
-            //unlink(path_c2s);
-            //unlink(path_s2c);
+            close(fd_c2s);
+            close(fd_s2c);
+            unlink(path_c2s);
+            unlink(path_s2c);
         } else {
-            printf("Welcome %s. Your balance is %s.\n", username, res);
+            printf("Welcome %s. Your balance is %s.", username, res);
+            while(1){
+                char cmd_buf[BUFF];
+                if (fgets(cmd_buf, BUFF, stdin) == NULL) break;
+                write(fd_c2s, cmd_buf, strlen(cmd_buf));
+                char res_buf[BUFF];
+                ssize_t res_size = read(fd_s2c, res_buf, sizeof(res_buf)-1);
+
+                if (res_size > 0){
+                    res_buf[res_size] = '\0';
+                    
+                }
+
+                }
+            }
         }
     }
     close(fd_c2s);
